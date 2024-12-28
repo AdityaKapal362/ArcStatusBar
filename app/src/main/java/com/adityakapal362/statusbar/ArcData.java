c00c719d305c9f6c74bd6c250c9c9890ef7a8c33
AdityaKapal362 <adityakapal362@gmail.com>
2024-12-28 14:50:30 +0700

    Create ArcData.java

---
+++ app/src/main/java/com/adityakapal362/statusbar/ArcData.java
@@ -0,0 +1,15 @@
+package com.adityakapal362.statusbar;
+
+public class ArcData {
+    
+	public boolean viewed;
+	
+	public ArcData() {
+		viewed = false;
+	}
+	
+	public ArcData(boolean a) {
+		viewed = a;
+	}
+    
+}
