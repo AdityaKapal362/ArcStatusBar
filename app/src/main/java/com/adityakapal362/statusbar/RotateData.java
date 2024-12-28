6ddf982820734c33e6d00d4e6d6931deff650c56
AdityaKapal362 <adityakapal362@gmail.com>
2024-12-28 14:51:08 +0700

    Create RotateData.java

---
+++ app/src/main/java/com/adityakapal362/statusbar/RotateData.java
@@ -0,0 +1,11 @@
+package com.adityakapal362.statusbar;
+
+public class RotateData {
+    
+	public boolean val, viewed;
+    
+	public RotateData(boolean a, boolean b) {
+		val = a;
+		viewed = b;
+	}
+}
